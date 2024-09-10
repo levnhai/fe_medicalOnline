8214356909966316d66d780c90eab6f3e497f224
le van hai <lvhai2k2@gmail.com>
2024-09-10 13:51:30 +0700

    font, add taiwindCss

---
+++ src/components/globalStyles/globalStyles.js
@@ -0,0 +1,6 @@
+import './globalStyles.module.scss';
+
+function GlobalStyles({ children }) {
+  return children;
+}
+export default GlobalStyles;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import GlobalStyles from './components/globalStyles';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GlobalStyles>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GlobalStyles>,
 );
 
 // If you want to start measuring performance in your app, pass a function
